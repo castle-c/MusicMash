6bbc5407deacf8d3bacaf167c6ff0c03e7ece27e
Castle Crawford <nathanscottcrawford@gmail.com>
2016-08-24 10:33:42 -0500

    created 404 shortcuts, created form and added form to template, created favorite function

--- music/urls.py
+++ music/urls.py
@@ -1,12 +1,18 @@
 from django.conf.urls import url
 from . import views #from this folder import views file
-
+app_name = 'music'
+#gives details,index html access to url by using 'music':'favorite' syntax
 
 urlpatterns = [
 #/music/
     url(r'^$', views.index, name='index'),
     #when music page loads go to views and run this function
 
-    #/music/99/
+    #/music/<albumid>/
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    #/music/<albumid>/favorite/
+    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+    #finds id of the song thatis selected and passes it to favorite url
+    # and sets is_favorite to true than redirects user to same page
+
 ]
--- music/views.py
+++ music/views.py
@@ -1,15 +1,30 @@
-from django.http import Http404
-from django.shortcuts import render
-from .models import Album
+from django.shortcuts import render, get_object_or_404
+from .models import Album, Song
 
 def index(request):
   all_albums = Album.objects.all()
-  context = { 'all_albums': all_albums }
-  return render(request, 'music/index.html', context)
+  indexContext = {'all_albums': all_albums}
+  return render(request, 'music/index.html', indexContext)
 
 def detail(request, album_id):
+  # album = Album.objects.get(pk=album_id)
+  album = get_object_or_404(Album, pk=album_id)
+  detailContext = {'album' : album}
+  return render(request, 'music/details.html', detailContext)
+
+def favorite(request, album_id):
+  album = get_object_or_404(Album, pk=album_id)
   try:
-    album = Album.objects.get(pk=album_id)
-  except Album.DoesNotExist:
-    raise Http404('Album not found')
-  return render(request, 'music/details.html', {'album' : album})
+    selected_song = album.song_set.get(pk=request.POST['song'])
+    # get value of whatever song is selected ^
+  except (KeyError, Song.DoesNotExist):
+    #http request for details html file
+    return render(request,'music/details.html', {
+      'album' : album,
+      'error_message': "please select a valid song",
+      })
+
+  else:
+    selected_song.is_favorite = True
+    selected_song.save()
+    return render(request, 'music/details.html', {'album' : album})
